e15ee7c96e35aeef2932fd0c6272608e92d2d9ea
Gianluca Gambari <gianluca.gambari95@gmail.com>
2018-12-23 15:00:16 +0100

    Update connect.php

--- database/connect.php
+++ database/connect.php
@@ -3,7 +3,6 @@
 function db_connection() {
 
     $con = mysqli_connect("localhost", "root", "root", "improve");
-    //$con = mysqli_connect("localhost", "S4100636", "giandanigian", "S4100636");
 
     if (mysqli_connect_errno($con)) {
         echo "Failed to connect to MySQL: " . mysqli_connect_error($con);
